ed901bfa3cd61fc379417fb1ca4b02cbbe2aee35
momoDragon <40727575+momoDragon@users.noreply.github.com>
2018-11-18 19:06:31 +0800

    re strucutre

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,26 +1,8 @@
-import random
-import pygame as py
-import randomwordgenerator as rwg
-import userInput as ui
-import outputDisplay as od
-import process as p
+import process
 
-num_words = 20
-x = rwg.generate_random_words(n = num_words)
-print(x)
 
-file = open("wordlist.txt","r")
-wordList = file.read().split(",")
-file.close()
-print(wordList)
+name = process.set_player_name()
+randomWord = process.random_word()
 
-guess_word = []
-secretWord = random.choice(wordList)
-length_word = len(secretWord)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-letter_storage = []
+print(randomWord)
 
-play = ui.Menu()
-
-if play == "y":
-    ui.Menu()
