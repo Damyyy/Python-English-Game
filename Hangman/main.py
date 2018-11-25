29911c986d9b906237159704a817c2de7525a660
momoDragon <40727575+momoDragon@users.noreply.github.com>
2018-11-26 00:29:56 +0800

    game title

--- Hangman/main.py
+++ Hangman/main.py
@@ -8,7 +8,7 @@ import outputDisplay
 # try running the code. If error occors we will print out message to inform user
 try:
     app = outputDisplay.MainMenu()
-    app.title("Game")
+    app.title("The Hardest Word Game Ever")
     app.geometry("800x500")
     # Main Loop
     app.mainloop()
